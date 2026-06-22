cc670a9a9a67c7cfd3fdabe5a32b145549232578
Daniel <daniel.frimu@hoculus.com>
2026-06-22 18:59:08 +0300

    return req from / route

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send({ message: 'It is working!' });
+    res.send({ message: 'It is working!', req: JSON.stringify(req, null, 2) });
 });
 
 app.listen(3000, () => console.log('App listening on port 3000'));
